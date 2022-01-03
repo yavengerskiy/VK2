cf82172f06b3c03e93746a0a8fcb7f882e60a9ad
yavengerskiy <yavengerskiy@yandex.ru>
2022-01-03 22:12:18 +0500

    Initial Commit

---
+++ VK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Beelab on 03/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
