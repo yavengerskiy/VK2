4cfc473048b17715f83fd0a3ba4c580d85818bc6
yavengerskiy <yavengerskiy@paymart.uz>
2022-02-11 00:00:48 +0500

    loginscreen

--- VK/LoginViewController.swift
+++ VK/LoginViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    private var currentUser: User!
     
     @IBOutlet var login: UITextField!
     @IBOutlet var password: UITextField!
@@ -19,37 +18,17 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func didLoginButtonPressed() {
-        
-    }
-    
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if checkForValidationData() {
-            guard let tabBarController = segue.destination as? UITabBarController else {return}
-            guard let tabBarControllers = tabBarController.viewControllers else {return}
             
-            for viewController in tabBarControllers {
-                guard let destinationNavigationController = viewController as? UINavigationController else {return}
-                let destVC = destinationNavigationController.topViewController
-                switch destVC {
-                case is FriendsTableViewController:
-                    let destController = destVC as! FriendsTableViewController
-                    destController.currentUser = currentUser
-                case is UserGroupTableViewController:
-                    let destController = destVC as! UserGroupTableViewController
-                    destController.currentUser = currentUser
-                default:
-                    break
-                }
-                
-            }
         }
     }
     
+    
+    
     private  func checkForValidationData() -> Bool {
         
-        if let currentUser = findUser(login: login.text ?? "", password: password.text ?? "") {
-            self.currentUser = currentUser
+        if login.text == "User", password.text ==  "123456" {
+            showAlert(title: "Congrats!", message: "All correct. To be continued ...")
             return true
         }
         showAlert(title: "Incorrect Login or Password 🥲", message: "Please try again with login: User \nand password: 123456 ")
@@ -67,19 +46,3 @@ extension LoginViewController {
         present(alert, animated: true, completion: nil)
     }
 }
-
-// MARK: - findUser
-extension LoginViewController {
-    private func findUser(login: String, password: String) -> User? {
-        let userList = DataManager.shared.createTempUsers()
-        for user in userList {
-            if user.login == login && user.password == password {
-                return user
-            }
-        }
-        return nil
-    }
-}
-
-
-
